e2a692f06dce4ff7a7462e258c4aae110e1064ab
Developer <jjstrain68@gmail.com>
2021-02-07 14:48:30 +0000

    Added primary qualifier for MyController and its service

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,14 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingsService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingsService greetingsService;
+
+    // No auto-wired required for constructor
+    public MyController(GreetingsService greetingsService) {
+        this.greetingsService = greetingsService;
+    }
+
     public String sayHello(){
 
-        System.out.println("Hello World");
-
-        return "Hi there";
+        return greetingsService.sayGreeting();
     }
 }
